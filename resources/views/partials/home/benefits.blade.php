956645e72d915aabf6e51b3a048f5c383b21543f
Sorin Tolocica <71135719+sorintolocica@users.noreply.github.com>
2026-04-19 01:15:39 +0300

    hidden arrows

--- resources/views/partials/home/benefits.blade.php
+++ resources/views/partials/home/benefits.blade.php
@@ -52,23 +52,6 @@
           </div>
         </div>
 
-        <button type="button"
-                class="home-slider__btn home-slider__btn--prev"
-                aria-label="{{ esc_attr__('Anterior', 'sage') }}"
-                data-home-slider-prev="benefits">
-          <svg width="22" height="22" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-            <path d="M15 18l-6-6 6-6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-        </button>
-        <button type="button"
-                class="home-slider__btn home-slider__btn--next"
-                aria-label="{{ esc_attr__('Următorul', 'sage') }}"
-                data-home-slider-next="benefits">
-          <svg width="22" height="22" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-            <path d="M9 18l6-6-6-6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-        </button>
-
         <div class="home-slider__pagination" data-home-slider-pagination="benefits"></div>
       </div>
     </section>
