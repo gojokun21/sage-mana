d1e3f9f378f89663ad1b1aaa2ca5b4a9c50095a7
Sorin Tolocica <71135719+sorintolocica@users.noreply.github.com>
2026-04-20 10:40:54 +0300

    last updates

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,3 +1,8 @@
 <div class="page-header">
   <h1>{!! $title !!}</h1>
+  @if (function_exists('is_cart') && is_cart() && ! WC()->cart->is_empty())
+    <a href="{{ esc_url(wc_get_checkout_url()) }}" class="page-header-checkout-btn">
+      {{ __('Finalizează comanda', 'sage') }}
+    </a>
+  @endif
 </div>
