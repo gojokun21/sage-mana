ddfcbcabd5fded5fe870050e96740a1d3d216e97
Sorin Tolocica <71135719+sorintolocica@users.noreply.github.com>
2026-05-08 11:47:27 +0300

    hidden block

--- resources/views/template-pachete.blade.php
+++ resources/views/template-pachete.blade.php
@@ -239,7 +239,7 @@
       ];
     @endphp
 
-    <section class="pachete-pasi" aria-labelledby="pachete-pasi-title">
+    <section class="pachete-pasi" aria-labelledby="pachete-pasi-title" style="display:none;">
       <div class="pachete-pasi__head">
         <span class="pachete-pasi__eyebrow">
           <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
