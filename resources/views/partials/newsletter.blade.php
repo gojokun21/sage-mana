1251c32c65b1aec45f7e41437af076323288d584
Sorin Tolocica <71135719+sorintolocica@users.noreply.github.com>
2026-05-11 17:03:40 +0300

    reverse changes

--- resources/views/partials/newsletter.blade.php
+++ resources/views/partials/newsletter.blade.php
@@ -4,20 +4,20 @@
   page-specific template. Embed is populated by Omnisend's external script.
 --}}
 
-{{--<section class="newsletter-strip" aria-labelledby="newsletter-title">--}}
-{{--  <div class="container">--}}
-{{--    <div class="newsletter-strip__inner">--}}
-{{--      <div class="newsletter-strip__intro">--}}
-{{--        <h2 id="newsletter-title" class="newsletter-strip__title">--}}
-{{--          {{ __('Abonează-te la newsletter', 'sage') }}--}}
-{{--        </h2>--}}
-{{--        <p class="newsletter-strip__text">--}}
-{{--          {{ __('Primește reduceri exclusive, sfaturi de la specialiști și noutăți despre produse naturale, direct pe e-mail.', 'sage') }}--}}
-{{--        </p>--}}
-{{--      </div>--}}
-{{--      <div class="newsletter-strip__form">--}}
-{{--        <div id="omnisend-embedded-v2-6a01d923ab28a246322eea14"></div>--}}
-{{--      </div>--}}
-{{--    </div>--}}
-{{--  </div>--}}
-{{--</section>--}}
+<section class="newsletter-strip" aria-labelledby="newsletter-title">
+  <div class="container">
+    <div class="newsletter-strip__inner">
+      <div class="newsletter-strip__intro">
+        <h2 id="newsletter-title" class="newsletter-strip__title">
+          {{ __('Abonează-te la newsletter', 'sage') }}
+        </h2>
+        <p class="newsletter-strip__text">
+          {{ __('Primește reduceri exclusive, sfaturi de la specialiști și noutăți despre produse naturale, direct pe e-mail.', 'sage') }}
+        </p>
+      </div>
+      <div class="newsletter-strip__form">
+        <div id="omnisend-embedded-v2-6a01d923ab28a246322eea14"></div>
+      </div>
+    </div>
+  </div>
+</section>
