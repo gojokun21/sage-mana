4354cf16e3b77b7a03ca7d94092f4e9e35aa1159
Sorin Tolocica <71135719+sorintolocica@users.noreply.github.com>
2026-05-15 13:17:11 +0300

    color categories

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ Application::configure()
 |
 */
 
-collect(['setup', 'filters', 'permalinks', 'ajax-search', 'woocommerce-tabs', 'mini-cart', 'cart-modal', 'favorites', 'cart', 'checkout', 'home', 'auth', 'bundle-stock', 'perf', 'acf-options', 'cpt-studii', 'datalayer'])
+collect(['setup', 'filters', 'permalinks', 'ajax-search', 'woocommerce-tabs', 'mini-cart', 'cart-modal', 'favorites', 'cart', 'checkout', 'home', 'auth', 'bundle-stock', 'perf', 'acf-options', 'cpt-studii', 'datalayer', 'category-theme'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
