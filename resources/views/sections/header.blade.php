1f239bf70790c4d8aacf8373c67dcc980849a2a1
Sorin Tolocica <71135719+sorintolocica@users.noreply.github.com>
2026-05-22 21:41:23 +0300

    10% menu

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -262,6 +262,9 @@
             <li class="{{ implode(' ', $li_classes) }}">
               <a href="{{ esc_url($item->url) }}">
                 {{ esc_html($item->title) }}
+                @if ($mega_type === 'pachete' && function_exists('App\\bf_is_live') && \App\bf_is_live())
+                  <span class="menu-promo-badge">-{{ \App\BF_PERCENT }}%</span>
+                @endif
                 @if ($has_mega)
                   <div class="mega-arrow">
                     <svg width="15" height="15" viewBox="0 0 24 24" aria-hidden="true">
@@ -300,6 +303,9 @@
                       @if ($mega_type === 'pachete')
                         <a href="{{ esc_url(home_url('/pachete/')) }}" class="mega-menu-cta">
                           <span>{{ __('Vezi toate pachetele', 'sage') }}</span>
+                          @if (function_exists('App\\bf_is_live') && \App\bf_is_live())
+                            <span class="menu-promo-badge">-{{ \App\BF_PERCENT }}%</span>
+                          @endif
                           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
                             <path d="M5 12 H19"/><path d="M13 6 L19 12 L13 18"/>
                           </svg>
